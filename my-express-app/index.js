d5697496f9661aae698a20fdc4e5af7d6e235d1a
Amani Magdi Farg <amanim.farag@gmail.com>
2024-09-24 16:54:01 +0300

    making simple NodeJS app with its Dockerfile

---
+++ my-express-app/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+//init app
+const PORT = 4000;
+const app = express();
+
+app.get('/', (req, res)=> res.send('<h1> Hello Tresmerge! </h1>'));
+app.listen(PORT, () => console.log(`app is up and running on port ${PORT}`));
