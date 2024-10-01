06910a150d2e83c70f9f72208ce9b919a3bf1089
Amani Magdi Farg <amanim.farag@gmail.com>
2024-10-01 13:51:31 +0300

    docker hot reload

--- my-express-app/index.js
+++ my-express-app/index.js
@@ -4,5 +4,5 @@ const express = require('express');
 const PORT = 4000;
 const app = express();
 
-app.get('/', (req, res)=> res.send('<h1> Hello Tresmerge! </h1>'));
+app.get('/', (req, res)=> res.send('<h1> Hello Tresmerge!</h1>'));
 app.listen(PORT, () => console.log(`app is up and running on port ${PORT}`));
